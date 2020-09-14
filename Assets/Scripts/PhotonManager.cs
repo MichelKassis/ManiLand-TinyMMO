aa57864b21804206d780c221384c0d427ef55801
Michel Kassis <michel.kassis120@gmail.com>
2020-09-14 17:13:21 -0400

    Initial commit

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+
+    //public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector2(Random.Range(-8f,11f),transform.position.y), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.SqrMagnitude());
+
+
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+    }
+}
